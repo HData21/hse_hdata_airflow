7eea7f5f4b0289e6c9fba0b69f0a4c25627c9c44
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-05-25 14:31:20 -0300

    Carga parcial dados HSE

--- dags/captura_dados.py
+++ dags/captura_dados.py
@@ -1344,11 +1344,11 @@ t7 = PythonOperator(
 #     on_failure_callback=notify_email,
 #     dag=dag)
 
-t14 = PythonOperator(
-    task_id="captura_paciente_hse",
-    python_callable=df_paciente,
-    on_failure_callback=notify_email,
-    dag=dag)
+# t14 = PythonOperator(
+#     task_id="captura_paciente_hse",
+#     python_callable=df_paciente,
+#     on_failure_callback=notify_email,
+#     dag=dag)
 
 # t15 = PythonOperator(
 #     task_id="captura_pagu_objeto_hse",
@@ -1411,4 +1411,4 @@ t23 = PythonOperator(
 #     dag=dag)
 
 #(t1, t3, t4, t5, t8, t9, t10, t11, t12, t13, t14, t15, t17, t18, t21, t22, t24) >> t16 >> t23 >> t20 >> t7 >> t2 >> t0
-t14 >> t16 >> t23 >> t20 >> t7 >> t2 >> t0
+t16 >> t23 >> t20 >> t7 >> t2 >> t0
