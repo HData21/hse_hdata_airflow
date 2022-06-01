2b51118516f9e9b5efe6d539828be3d53356d866
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-01 12:30:20 -0300

    Get data VARCLIN 2021 HSE

--- dags/captura_dados.py
+++ dags/captura_dados.py
@@ -59,7 +59,7 @@ def update_cells(df_eq, table_name, CD):
 
 def df_atendime():
     print("Entrou no df_atendime")
-    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2021, 1, 1), until=datetime.datetime(2021, 12, 31)):
+    for dt in rrule.rrule(rrule.DAILY, dtstart=dt_ini, until=dt_ontem):
         data_1 = dt
         data_2 = dt
 
